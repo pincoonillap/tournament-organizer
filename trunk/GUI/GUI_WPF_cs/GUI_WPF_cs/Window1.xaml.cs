044ba3de5eb7f9773e26ab9f0459a6fd689a1066
alex.wollman@gmail.com <alex.wollman@gmail.com@b71d7900-c59d-11de-afa3-455a29e9879b>
2009-12-12 09:12:37 +0000

    Created a file opening system.  Not perfect but its a start.  Added a utility to aid us, inform me (Alex) if you have problems.

--- trunk/GUI/GUI_WPF_cs/GUI_WPF_cs/Window1.xaml.cs
+++ trunk/GUI/GUI_WPF_cs/GUI_WPF_cs/Window1.xaml.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,65 +10,55 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.IO;
+using WPF.MDI;
+using GUI_WPF_cs.Content;
 
-// NOTE: The original concept of this demo was created by Sacha Barber, in
-// this article: http://www.codeproject.com/useritems/WPF_Explorer_Tree.asp
-
-namespace WpfExplorerTreeNoConverter
+namespace GUI_WPF_cs
 {
-	/// <summary>
-	/// Interaction logic for Window1.xaml
-	/// </summary>
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
 
-	public partial class Window1 : System.Windows.Window
-	{
-		private readonly object _dummyNode = null;
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Add(new MdiChild()
+            {
+                Title = "Window Home",
+                Name = "WindowHome",
+                Height = 549,
+                Width = 642,
+                Content = new stuffHome()
+            });
+        }
 
-		public Window1()
-		{
-			InitializeComponent();
-			this.Loaded += new RoutedEventHandler(Window1_Loaded);
-		}
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Add(new MdiChild()
+            {
+                Title = "Window New",
+                Name = "WindowNew",
+                Height = 468,
+                Width = 463,
+                Content = new stuffNew()
+            });
+        }
 
-		void Window1_Loaded(object sender, RoutedEventArgs e)
-		{
-			foreach (string drive in Directory.GetLogicalDrives())
-			{
-				TreeViewItem item = new TreeViewItem();
-				item.Header = drive;
-				item.Tag = drive;
-				item.Items.Add(_dummyNode);
-				item.Expanded += folder_Expanded;
-
-				// Apply the attached property so that 
-				// the triggers know that this is root item.
-				TreeViewItemProps.SetIsRootLevel(item, true);
-
-				foldersTree.Items.Add(item);
-			}
-		}
-
-		void folder_Expanded(object sender, RoutedEventArgs e)
-		{
-			TreeViewItem item = (TreeViewItem)sender;
-			if (item.Items.Count == 1 && item.Items[0] == _dummyNode)
-			{
-				item.Items.Clear();
-				try
-				{					
-					foreach (string dir in Directory.GetDirectories(item.Tag as string))
-					{
-						TreeViewItem subitem = new TreeViewItem();
-						subitem.Header = new DirectoryInfo(dir).Name;
-						subitem.Tag = dir;
-						subitem.Items.Add(_dummyNode);
-						subitem.Expanded += folder_Expanded;
-						item.Items.Add(subitem);
-					}
-				}
-				catch (Exception) { }
-			}
-		}
-	}
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            MainContent.Children.Add(new MdiChild()
+            {
+                Title = "Window Open",
+                Name = "WindowOpen",
+                Height = 468,
+                Width = 463,
+                Content = new stuffOpen()
+            });
+        }
+    }
 }
