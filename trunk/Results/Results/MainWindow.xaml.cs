4da09ae9aaa8cc88a2d0328d04c4689d33600052
tcsteinle <tcsteinle@b71d7900-c59d-11de-afa3-455a29e9879b>
2009-12-13 18:57:08 +0000

    Added pages for player database and tournament results.

---
+++ trunk/Results/Results/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WpfApplication1.PlayerInfoDataSet PlayerInfoDataSet = ((WpfApplication1.PlayerInfoDataSet)(this.FindResource("PlayerInfoDataSet")));
+            // Load data into the table Total_Results. You can modify this code as needed.
+            WpfApplication1.PlayerInfoDataSetTableAdapters.Total_ResultsTableAdapter playerInfoDataSetTotal_ResultsTableAdapter = new WpfApplication1.PlayerInfoDataSetTableAdapters.Total_ResultsTableAdapter();
+            playerInfoDataSetTotal_ResultsTableAdapter.Fill(PlayerInfoDataSet.Total_Results);
+            System.Windows.Data.CollectionViewSource total_ResultsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("total_ResultsViewSource")));
+            total_ResultsViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void total_ResultsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ trunk/Results/Results/TourneyResults.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            WpfApplication1.PlayerInfoDataSet PlayerInfoDataSet = ((WpfApplication1.PlayerInfoDataSet)(this.FindResource("PlayerInfoDataSet")));
+            // Load data into the table Total_Results. You can modify this code as needed.
+            WpfApplication1.PlayerInfoDataSetTableAdapters.Total_ResultsTableAdapter playerInfoDataSetTotal_ResultsTableAdapter = new WpfApplication1.PlayerInfoDataSetTableAdapters.Total_ResultsTableAdapter();
+            playerInfoDataSetTotal_ResultsTableAdapter.Fill(PlayerInfoDataSet.Total_Results);
+            System.Windows.Data.CollectionViewSource total_ResultsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("total_ResultsViewSource")));
+            total_ResultsViewSource.View.MoveCurrentToFirst();
+            WpfApplication1.TournamentDataSet TournamentDataSet = ((WpfApplication1.TournamentDataSet)(this.FindResource("TournamentDataSet")));
+            // Load data into the table Tournament_Results. You can modify this code as needed.
+            WpfApplication1.TournamentDataSetTableAdapters.Tournament_ResultsTableAdapter tournamentDataSetTournament_ResultsTableAdapter = new WpfApplication1.TournamentDataSetTableAdapters.Tournament_ResultsTableAdapter();
+            tournamentDataSetTournament_ResultsTableAdapter.Fill(TournamentDataSet.Tournament_Results);
+            System.Windows.Data.CollectionViewSource tournament_ResultsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tournament_ResultsViewSource")));
+            tournament_ResultsViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void tournament_ResultsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
